e3c519128d08a2de798661bf5b0dff65b2a37191
Veronica-Alfr <veagalves@gmail.com>
2022-03-22 16:43:52 -0300

    Verificado se links redirecionam para suas respectivas paginas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes sobre as páginas e Api de Pókemons', () => {
+  test('Testa os links do componente App.js', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorites);
+    expect(linkFavorites).toBeInTheDocument();
+  });
+});
