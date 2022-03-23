d89a17293bcf910dda46a4eb6529ad8986103ec7
Veronica-Alfr <veagalves@gmail.com>
2022-03-22 21:10:42 -0300

    Component NotFound testado e referencias adds

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,15 +4,17 @@ import { FavoritePokemons } from '../components';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
 
-describe('Testes do componente About', () => {
+describe('Testes do componente FavoritePokemons', () => {
   test('Testa se mensagem aparece na tela quando não há pokemons', () => {
     renderWithRouter(<FavoritePokemons pokemons={ [] } />);
     const notFoundPoke = screen.getByText('No favorite pokemon found');
     expect(notFoundPoke).toBeInTheDocument();
   });
+  // Ajuda de Vitu na mentoria de 22/03 com a lógica do retorno da prop.
   test('Testa se há pokemons na tela', () => {
     const pokeFilter = pokemons.filter((pokemon) => pokemon.name === 'Pikachu');
     renderWithRouter(<FavoritePokemons pokemons={ pokeFilter } />);
     expect(pokeFilter[0].type).toBe('Electric');
   });
+  // Ajuda de Gustavo Rondello na lógica para ser usado uma HOF.
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes do componente NotFound', () => {
+  test('Testa o conteúdo de heading do NotFound.js', () => {
+    renderWithRouter(<NotFound />);
+    const headingNotFound = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+  // Ajuda de Gustavo Rondello na lógica para ser usado regex ao invés de vários getByRole.
+  test('Testa os atributos da img do NotFound.js', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
