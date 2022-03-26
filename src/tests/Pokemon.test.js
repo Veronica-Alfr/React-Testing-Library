dd71370d80ddc9007b60017f7784fe142bf8f15b
Veronica-Alfr <veagalves@gmail.com>
2022-03-25 22:02:57 -0300

    Requisito 6 finalizando

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import App from '../App';
 import { Pokemon } from '../components';
 import renderWithRouter from './renderWithRouter';
 
@@ -8,18 +9,43 @@ const justPikachu = require('./mockPikachu');
 
 describe('Testes do componente Pokemon.js', () => {
   test('se é renderizado as informações de determinado pokemon', () => {
-    const isFavorite = {
-      25: true,
-    };
-    const withLink = true;
     renderWithRouter(<Pokemon
-      isFavorite={ isFavorite }
-      pokemon={ justPikachu }
-      showDetailsLink={ withLink }
+      pokemon={ justPikachu[0] }
+      showDetailsLink
+      isFavorite
     />);
-    console.log(justPikachu);
     const dataIdName = screen.getByTestId('pokemon-name');
     console.log(dataIdName);
     expect(dataIdName).toHaveTextContent('Pikachu');
+
+    const dataIdType = screen.getByTestId('pokemon-type');
+    expect(dataIdType).toHaveTextContent('Electric');
+
+    const dataIdWeigth = screen.getByTestId('pokemon-weight');
+    expect(dataIdWeigth).toHaveTextContent('Average weight: 6.0 kg');
+
+    const image = screen.getByRole('img', { name: 'Pikachu sprite' });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  test('se há um link de navegação para exibir detalhes deste Pokémon', () => {
+    renderWithRouter(<App />);
+    const pokemonLink = screen.getByRole('link', { name: 'More details' });
+    expect(pokemonLink).toBeInTheDocument();
+    expect(pokemonLink).toHaveAttribute('href', '/pokemons/25');
+
+    userEvent.click(pokemonLink);
+    const subtitle = screen.getByRole('heading', { name: 'Summary', level: 2 });
+    expect(subtitle).toBeInTheDocument();
+  });
+  test('se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    renderWithRouter(<Pokemon
+      pokemon={ justPikachu[0] }
+      showDetailsLink
+      isFavorite
+    />);
+    const imgFavorite = screen.getByRole('img', { name: /Pikachu is marked/i });
+    expect(imgFavorite).toBeInTheDocument();
+    expect(imgFavorite).toHaveAttribute('src', '/star-icon.svg');
   });
 });
